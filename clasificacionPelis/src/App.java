0e0ff90c1d87cfbdeec6ce9d0f12c800ba28e356
Azahinu <kanekipop2@gmail.com>
2022-08-24 23:39:53 -0500

    "Creacion de repositorio"

--- clasificacionPelis/src/App.java
+++ clasificacionPelis/src/App.java
@@ -2,6 +2,6 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        JOptionPane.showMessageDialog(null, "Hola mundo ");
+        JOptionPane.showMessageDialog(null, "Hola mundo  ");
     }
 }
