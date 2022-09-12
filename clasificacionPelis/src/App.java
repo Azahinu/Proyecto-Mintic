66587217fcc5859ccf57df196e324dd283f2c446
Azahinu <kanekipop2@gmail.com>
2022-09-11 22:52:25 -0500

    Cambios en el backend, se ingreso el sprint boot para crear la pagina web, se utilizo jdk 17

--- clasificacionPelis/src/App.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        JOptionPane.showMessageDialog(null, "Hola mundo  ");
-    }
-}
